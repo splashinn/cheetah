5e9e3d7bae3cca92850a3b3f5b2813958b70f985
kyle <the.kyle.maune@gmail.com>
2017-07-16 08:04:29 -0700

    added help and donate pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,10 @@
 class PagesController < ApplicationController
+  def help
+    
+  end
+
+  def donate
+
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root to: 'dashboard#index'
 
+  get 'pages/help'
+  get 'pages/donate'
+
 end
